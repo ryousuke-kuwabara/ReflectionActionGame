48860b8af19992d21198ad21e4a61c9db47b5aca
nekokoneko03 <ryousuke1039@gmail.com>
2024-07-15 12:59:04 +0900

    Add google admob package and add banner ads.

--- Assets/Scripts/Admob/AdmobBanner.cs
+++ Assets/Scripts/Admob/AdmobBanner.cs
@@ -1,18 +1,18 @@
-/*
- * using GoogleMobileAds.Api;
+using GoogleMobileAds.Api;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AdmobBanner : MonoBehaviour
 {
-#if UNITY_ANDROID
-    private string _adUnitId = "ca-app-pub-8762130636007787/8190542983";
-#elif UNITY_IPHONE
-    private string _adUnitId = "ca-app-pub-8762130636007787/9312052962";
-#else
+# if UNITY_ANDROID
+    private string _adUnitId = "ca-app-pub-5810037512419048/8509983476";
+# elif UNITY_IPHONE
+    private string _adUnitId = "ca-app-pub-3940256099942544/2934735716";
+# else
     private string _adUnitId = "unused";
-#endif
+# endif
 
     private BannerView _bannerView;
 
@@ -23,6 +23,11 @@ public class AdmobBanner : MonoBehaviour
 
         });
 
+        RequestConfiguration requestConfiguration = new RequestConfiguration();
+        requestConfiguration.TestDeviceIds.Add("B9CF6CC5410F2E4AE26EF852C3597145");
+
+        MobileAds.SetRequestConfiguration(requestConfiguration);
+
         CreateBannerView();
         LoadAd();
     }
@@ -31,13 +36,14 @@ public class AdmobBanner : MonoBehaviour
     {
         if (_bannerView != null)
         {
-            DestroyAd();
+            this.DestroyAd();
         }
 
         AdSize adaptiveSize =
             AdSize.GetCurrentOrientationAnchoredAdaptiveBannerAdSizeWithWidth(AdSize.FullWidth);
 
         _bannerView = new BannerView(_adUnitId, adaptiveSize, AdPosition.Bottom);
+        _bannerView.OnBannerAdLoaded += this.HandleBannerAdLoaded;
     }
 
     private void LoadAd()
@@ -51,5 +57,9 @@ public class AdmobBanner : MonoBehaviour
         _bannerView.Destroy();
         _bannerView = null;
     }
+
+    public void HandleBannerAdLoaded()
+    {
+        _bannerView.Show();
+    }
 }
-*/
