ecb67b80de3a1d0a4c3fa991788adbe2b3272143
ryousuke <ryousuke1039@gmail.com>
2024-11-06 21:12:36 +0900

    feat: Adjust item spawn position based on baby position

--- Assets/Scripts/ItemSpawner.cs
+++ Assets/Scripts/ItemSpawner.cs
@@ -9,6 +9,9 @@ public class ItemSpawner : MonoBehaviour
 
     [SerializeField] private float spawnDelayTime;
 
+    [SerializeField] private Transform _playerTransform;
+    [SerializeField] private float _minimumDistanceFromPlayer;
+
     private void Start()
     {
         StartCoroutine(SpawnItem());
@@ -20,11 +23,28 @@ public class ItemSpawner : MonoBehaviour
         {
             yield return new WaitForSeconds(spawnDelayTime);
 
-            Vector3 spawnPosition = stage.GenerateRandomPositionInRadius();
+            Vector3 spawnPosition = GetValidSpawnPosition();
             int spwanItemindex = Random.Range(0, itemList.Count);
 
             Item newItem =
                 Instantiate(itemList[spwanItemindex], spawnPosition, Quaternion.identity);
         }
+    }
+
+    private Vector3 GetValidSpawnPosition()
+    {
+        Vector3 spawnPosition;
+        float currentDistance;
+
+        do
+        {
+            spawnPosition = stage.GenerateRandomPositionInRadius();
+            currentDistance = Vector3.Distance(spawnPosition, _playerTransform.position);
+
+            if (currentDistance >= _minimumDistanceFromPlayer)
+            {
+                return spawnPosition;
+            }
+        } while (true);
     }
 }
